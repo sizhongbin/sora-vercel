5d86f1b14366f7fe3228a428ea7bfa9df36728ad
sizhongbin <sizhongbin@hotmail.com>
2024-10-30 23:30:53 +0800

    Add static hosting

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+// Index hosting
+server.use(express.static('public'));
+
 // Routers
-import account from './routes/account.js';
+const account = require('./routes/account.js');
 server.use('/api/account', account);
 
-server.listen(8080, () =>
-  console.log(`Server running on: http://localhost:8080`)
+server.listen(8081, () =>
+  console.log(`Server running on: http://localhost:8081`)
 );
 
 module.exports = server;
